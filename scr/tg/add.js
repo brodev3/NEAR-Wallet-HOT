951f630f1ee3ea7446fa005bb1b5371c8c84c49d
BRO.DEV <fassas47@gmail.com>
2024-05-17 20:43:13 +0700

    /

--- scr/tg/add.js
+++ scr/tg/add.js
@@ -8,7 +8,7 @@ const utils = require("../utils/utils");
     let listAccounts = Object.keys(data);
     for (let account of listAccounts){
         let accountData = data[account];
-        log.info("Adding data to " + accountData.username);
+        log.info("Adding data to " + account);
         let token = await input.text("Hot token?");
         let privateKey = await input.text("privateKey?");
         data[account]["hotToken"] = token;
